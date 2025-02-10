5892ffe8707e15dc5be887487bc9f40bacb097c2
edurs2602 <edurs.2602@gmail.com>
2025-02-10 01:27:25 -0300

    fix: adjusting the get request to return the correct infos

--- automotivas/part/serializers.py
+++ automotivas/part/serializers.py
@@ -2,8 +2,21 @@ from rest_framework import serializers
 from .models import Part
 from car.models import Car
 
+class ListPartSerializer(serializers.ModelSerializer):
+    """
+    Serializer para listagem de peças.
+    Retorna apenas: part_number, name, details, price e quantity.
+    """
 
-class PartSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Part
+        fields = ['id', 'part_number', 'name', 'details', 'price', 'quantity']
+
+class PartDetailSerializer(serializers.ModelSerializer):
+    """
+    Serializer detalhado para Part.
+    Retorna todas as informações, inclusive updated_at e os modelos associados.
+    """
     car_models = serializers.PrimaryKeyRelatedField(
         many=True,
         queryset=Car.objects.all(),
